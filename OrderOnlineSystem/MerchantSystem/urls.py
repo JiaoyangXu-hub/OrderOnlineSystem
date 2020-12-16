680752e32351a106f299158d3be64a789406a260
JiaoyangXu-hub <x601624589@163.com>
2020-12-17 00:46:47 +0800

    基本完成商家和顾客两个子系统,准备做送餐员系统,但是小费的一些流程没有设置,需要复盘代码再做

--- OrderOnlineSystem/MerchantSystem/urls.py
+++ OrderOnlineSystem/MerchantSystem/urls.py
@@ -13,5 +13,7 @@ urlpatterns = [
     path('Dish/<int:dishID>/',views.dishDetail),# 维护菜品请求，返回一个维护网页
     path('Dish/<int:dishID>/updateDish_post/',views.updateDish_post),#修改菜品post表单提交
     path('DelDish/<int:dishID>/',views.delDish),# 删除菜品请求
+    path('dealItem/<int:orderID>/',views.dealItem_get),# 处理接单请求
+
 ]
 
--- OrderOnlineSystem/MerchantSystem/views.py
+++ OrderOnlineSystem/MerchantSystem/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect,HttpResponse
-from cmdb.models import Menu,Usr,Order
+from cmdb.models import Menu,Usr,Order,Bill,WaitCash
 from Form import MerchantDish
 # Create your views here.
 
@@ -14,17 +14,15 @@ def base_view(request):
     else:
         return redirect('/Login/')
 
-def getCash_view(request):
-    """
-    提款成功界面
-    """
-    return render(request,'M/getCash_view.html')
 
 def dealItem_view(request):
     """
-    制作餐品
+    查看历史订单,确认制作餐品
     """
-    return render(request, "M/dealItem_view.html")
+    user = Usr.objects.get(ID=request.session['user_id'])
+    # 该商家的订单,去掉待支付的项
+    orders = Order.objects.filter(MerchantID=user).exclude(stage='待支付')
+    return render(request, "M/dealItem_view.html",{'orders':orders,'user_id':request.session['user_id']})
 
 def addDish(request):
     """
@@ -47,7 +45,6 @@ def addDish_post(request):
         print(dish_form.errors)
         return HttpResponse(str(dish_form.errors))
 
-
 def dishDetail(request,dishID):
     """
     展示餐品的评论以及维护餐品(修改或删除)
@@ -56,7 +53,6 @@ def dishDetail(request,dishID):
     dish_form = MerchantDish()
     comment = Order.objects.filter(itemID=dishID).values('commentMerchant')
     return render(request,'M/detailDish.html',{'form':dish_form,'comment':comment,'dishID':dishID})
-
 
 def delDish(request,dishID):
     """
@@ -77,3 +73,28 @@ def updateDish_post(request,dishID):
     elif dish_form.errors is not None:
         print(dish_form.errors)
         return HttpResponse(str(dish_form.errors))
+
+
+def getCash_view(request):
+    """
+    点击提现按钮后，弹出提现提示页面
+    """
+    user = Usr.objects.get(ID=request.session['user_id'])
+    bill = Bill.objects.filter(usrID=user)
+    totalCount=0
+    for item in bill:
+        totalCount += item.price
+    # 创建待提现表中的数据项
+    WaitCash.objects.create(price=totalCount,usrID=user,note='等待提现')
+    # 删除这些账单项
+    Bill.objects.filter(usrID=user).delete()
+    return render(request,'M/getCash_view.html',{'total':totalCount,'user_id':request.session['user_id']})
+
+
+def dealItem_get(request,orderID):
+    '''
+    响应接单请求,并变换Order表的stage状态
+    '''
+    Order.objects.filter(orderID=orderID).update(stage='制作中')
+    return redirect('/MerchantSystem/dealItem/')
+
