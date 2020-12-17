f6f83601eb62d8b6ccfec407a497d2a556654783
JiaoyangXu-hub <x601624589@163.com>
2020-12-17 18:54:32 +0800

    cmdb.admin上面还需要构造一个admin的控制，具体可参考教程。没做完..

--- OrderOnlineSystem/cmdb/admin.py
+++ OrderOnlineSystem/cmdb/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Usr,Order,Bill,WaitCash,Menu
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    """
+    建立用户表的修改
+    """
+    # 未完待续
+
